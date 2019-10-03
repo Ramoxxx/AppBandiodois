06d634d4e2d1be64012ebee2eb9d083c4c9660dd
Maowss <Maowss@gmail.com>
2019-10-02 21:56:27 -0300

    lam bexota

---
+++ app/src/main/java/br/com/etecia/appbandio/signup_layout.java
@@ -0,0 +1,14 @@
+package br.com.etecia.appbandio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class signup_layout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup_layout);
+    }
+}
